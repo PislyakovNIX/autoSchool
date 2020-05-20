6b985cdc5b9be9328ee6ca5f1e34d6cee8992697
pislyakov <ruslan.pislyakov@nixsolution.com>
2020-05-20 11:48:05 +0300

    Changes in TypeConversion_Ex_3

--- src/main/java/TypeConversion_Ex_3.java
+++ src/main/java/TypeConversion_Ex_3.java
@@ -1,14 +1,11 @@
-import java.io.IOException;
 
 // В переменной n хранится вещественное число с ненулевой дробной частью. Создайте программу, округляющую число n до ближайшего целого и выводящую результат на экран.
 
 public class TypeConversion_Ex_3 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         double n = 3.94;
-        int res = (int)n;
+        int res = (int) n;
         System.out.print(res);
-
     }
-
 }
