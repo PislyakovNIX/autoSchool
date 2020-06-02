5385fb2a13aa44cdbd2537a67b96625faadbe844
pislyakov <ruslan.pislyakov@nixsolution.com>
2020-06-02 14:23:56 +0300

    LogExer_4x3

--- src/main/java/figur_3x6/Main.java
+++ src/main/java/figur_3x6/Main.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         GeometryFig[] figureList = new GeometryFig[10]; // Это массив фигур
         Random rnd = new Random();
 
